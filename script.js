2fd5d6161178a2ab512d413c3c5b5834050b2655
Cagla Sezer <caglasezer2002@gmail.com>
2024-06-24 13:28:42 +0300

    navbar eklendi

---
+++ script.js
@@ -0,0 +1,10 @@
+const videoText = document.querySelector('.video-text');
+
+
+//Video üstündeki yazı animasyon 
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        videoText.classList.add('show');
+    }, 500); 
+  });
+  

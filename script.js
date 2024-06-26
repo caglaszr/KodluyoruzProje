3c74378754fd54dd96cf81b5e1320c8a7913c0b5
Cagla Sezer <caglasezer2002@gmail.com>
2024-06-26 15:00:37 +0300

    blog1 sayfası eklendi

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const videoText = document.querySelector('.video-text');
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+const panels = document.querySelectorAll('.panel')
 
 
 //Video üstündeki yazı animasyon 
@@ -8,3 +11,34 @@ document.addEventListener('DOMContentLoaded', function() {
     }, 500); 
   });
   
+
+  //scroll
+window.onscroll = function() {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  }
+  scrollToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  });
+
+  
+// expanding
+panels.forEach((panel)=>{
+    panel.addEventListener('click',()=>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
